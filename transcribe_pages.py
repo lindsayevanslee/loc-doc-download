1e086491e4a251adca9eba1702870d17097234c8
Lindsay Lee <yasdnil922@comcast.net>
2024-09-11 20:06:12 -0500

    Cycle through all issues of the publication

--- transcribe_pages.py
+++ transcribe_pages.py
@@ -3,7 +3,12 @@ import xml.etree.ElementTree as ET
 from pdf2image import convert_from_path
 import pytesseract #Requires Tesseract-OCR to be installed. Installed with Homebrew with `brew install tesseract`
 from PIL import Image
+import sys
 
+# Path to the directory containing the publication issues
+pub_path = "downloads/The Rugbeian and District Reporter (Rugby, Tenn.) 1882 to 1883 (25)"
+
+# Obtain OCR data for a pdf file
 def process_pdf(pdf_path, alto_path):
     # Convert PDF to image (assuming only one image)
     image = convert_from_path(pdf_path)[0]
@@ -27,6 +32,7 @@ def process_pdf(pdf_path, alto_path):
     with open(pdf_path.replace(".pdf", "_ocr.txt"), "w", encoding="utf-8") as f:
         f.write(refined_text)
 
+# Extract text content from ALTO XML structure
 def extract_text_from_alto(root, scale_factor):
     namespace = {'alto': 'http://www.loc.gov/standards/alto/ns-v2#'}
     refined_text = ""
@@ -49,19 +55,31 @@ def extract_text_from_alto(root, scale_factor):
     
     return refined_text
 
-def main():
-    page_folder = "downloads/The Rugbeian and District Reporter (Rugby, Tenn.) 1882 to 1883 (25)/1882-10-07"
-    
-    for filename in os.listdir(page_folder):
-        if filename.endswith(".pdf"):
-            pdf_path = os.path.join(page_folder, filename)
-            alto_path = os.path.join(page_folder, filename.replace(".pdf", ".xml"))
-            
-            if os.path.exists(alto_path):
-                print(f"Processing {filename}")
-                process_pdf(pdf_path, alto_path)
-            else:
-                print(f"ALTO XML not found for {filename}")
+# Run transcription for all PDF files in a directory
+def extract_all_text_from_alto(publication_path):
 
-if __name__ == "__main__":
-    main()
+    if not os.path.isdir(publication_path):
+        print(f"Error: {publication_path} is not a valid directory.")
+        sys.exit(1)
+
+    print(f"Processing {publication_path}")
+
+    for issue_folder in os.listdir(publication_path):
+        issue_path = os.path.join(publication_path, issue_folder)
+
+        print(f"Processing {issue_folder}")
+
+        if os.path.isdir(issue_path):
+            for filename in os.listdir(issue_path):
+                if filename.endswith(".pdf"):
+                    pdf_path = os.path.join(issue_path, filename)
+                    alto_path = os.path.join(issue_path, filename.replace(".pdf", ".xml"))
+
+                    if os.path.exists(alto_path):
+                        print(f"Processing {filename} in {issue_folder}")
+                        process_pdf(pdf_path, alto_path)
+                    else:
+                        print(f"ALTO XML not found for {filename} in {issue_folder}")
+
+
+extract_all_text_from_alto(pub_path)
